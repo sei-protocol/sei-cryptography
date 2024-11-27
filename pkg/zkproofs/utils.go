e864733f41829a45a887610011ea91b7a0a1d072
mj <riku850@gmail.com>
2024-11-27 02:30:41 -0500

    explain

--- pkg/zkproofs/utils.go
+++ pkg/zkproofs/utils.go
@@ -7,9 +7,11 @@ import (
 	"github.com/coinbase/kryptology/pkg/core/curves"
 )
 
+// Generates a non-zero random scalar. The chances of generating a zero scalar are very low.
 func GenerateRandomScalar(curve *curves.Curve) (curves.Scalar, error) {
 	attempts := 0
 	scalar := curve.Scalar.Random(rand.Reader)
+	// Try 5 times to generate a non zero scalar. The chance that this fails with a normal random number generator is impossibly low.
 	for scalar.IsZero() && attempts < 5 {
 		curve.Scalar.Random(rand.Reader)
 		attempts += 1
