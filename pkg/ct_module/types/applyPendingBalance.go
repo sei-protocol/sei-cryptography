643268c81e84955940ebb818facb9613fca2d69a
mj <riku850@gmail.com>
2025-04-15 18:00:47 -0400

    another test

--- pkg/ct_module/types/applyPendingBalance.go
+++ pkg/ct_module/types/applyPendingBalance.go
@@ -1,6 +1,7 @@
 package types
 
 import (
+	"fmt"
 	"math/big"
 
 	"github.com/sei-protocol/sei-cryptography/pkg/encryption"
@@ -26,23 +27,27 @@ func NewApplyPendingBalance(
 	currentAvailableBalance,
 	currentPendingBalanceLo,
 	currentPendingBalanceHi *elgamal.Ciphertext) (*ApplyPendingBalance, error) {
+	fmt.Println("TEST 1")
 	aesKey, err := encryption.GetAESKey(signedDenom)
 	if err != nil {
 		return nil, err
 	}
 
+	fmt.Println("TEST 2")
 	// Get the current balance from the decryptable balance.
 	currentBalance, err := encryption.DecryptAESGCM(currentDecryptableBalance, aesKey)
 	if err != nil {
 		return nil, err
 	}
 
+	fmt.Println("TEST 3")
 	teg := elgamal.NewTwistedElgamal()
 	keyPair, err := teg.KeyGen(signedDenom)
 	if err != nil {
 		return nil, err
 	}
 
+	fmt.Println("TEST 4")
 	// Calculate the pending balances that we need to add to the available balance.
 	loBalance, err := teg.DecryptLargeNumber(keyPair.PrivateKey, currentPendingBalanceLo, elgamal.MaxBits32)
 	if err != nil {
@@ -60,12 +65,14 @@ func NewApplyPendingBalance(
 	// Sum the balances to get the new available balance
 	newDecryptedAvailableBalance := new(big.Int).Add(currentBalance, pendingBalance)
 
+	fmt.Println("TEST 5")
 	// Encrypt the new available balance
 	newDecryptableAvailableBalance, err := encryption.EncryptAESGCM(newDecryptedAvailableBalance, aesKey)
 	if err != nil {
 		return nil, err
 	}
 
+	fmt.Println("TEST 6")
 	return &ApplyPendingBalance{
 		Address:                        address,
 		Denom:                          denom,
